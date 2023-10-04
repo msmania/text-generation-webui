39356e3d97a05fdf90b1d746c3d445037b06dc4b
tokikuch <k.toshihito@yahoo.de>
2023-10-03 23:40:17 -0700

    SSL Reverse SSH Tunneling

--- start_rpc_server.sh
+++ start_rpc_server.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-MODEL=$1
 MODEL_DIR=${MODEL/\//_}
+
+echo Connecting to ${TUNNEL_SERVER}...
+
+ssh -f -N -T \
+  -o StrictHostKeyChecking=no \
+  -R ${TUNNEL_REMOTE_PORT}:localhost:${OPENEDAI_PORT} \
+  -i "/app/ssh-mount/${TUNNEL_KEY_NAME}" \
+  ${TUNNEL_SERVER}
+RET_SSH=$?
+
+if [ ${RET_SSH} -ne 0 ]; then
+  echo Cannot establish the channel to ${TUNNEL_SERVER}
+  tail -f /dev/null
+fi
+
+echo Established the channel to ${TUNNEL_SERVER}
+
+if [[ "${MODEL}" == "NULL" ]]; then
+  tail -f /dev/null
+fi
 
 if [ ! -d "models/${MODEL_DIR}" ]; then
   python3 download-model.py "${MODEL}"
 fi
 
-python3 server.py \
-  --model ${MODEL_DIR} \
-  ${@:2}
+python3 server.py --model ${MODEL_DIR} $@
