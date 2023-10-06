3241c476f1c8e41d2ee9c66edd408795d9fd0970
tokikuch <k.toshihito@yahoo.de>
2023-10-06 09:07:36 -0700

    Enable WebSocket streaming

--- start_rpc_server.sh
+++ start_rpc_server.sh
@@ -2,26 +2,44 @@
 
 MODEL_DIR=${MODEL/\//_}
 
-if [[ "${TUNNEL_KEY_NAME}" != "" ]]; then
-  echo Connecting to ${TUNNEL_SERVER}...
+[ -f "/app/${TUNNEL_KEY_NAME}" ] || \
+  cp "/app/ssh-mount/${TUNNEL_KEY_NAME}" /app/ \
+  && chmod 400 "/app/${TUNNEL_KEY_NAME}"
 
-  [ -f "/app/${TUNNEL_KEY_NAME}" ] || \
-    cp "/app/ssh-mount/${TUNNEL_KEY_NAME}" /app/ \
-    && chmod 400 "/app/${TUNNEL_KEY_NAME}"
+if [[ "${TUNNEL_REMOTE_PORT_RPC}" != "" ]]; then
+  echo Connecting to ${TUNNEL_SERVER_RPC}...
 
   ssh -f -N -T \
     -o StrictHostKeyChecking=no \
-    -R ${TUNNEL_REMOTE_PORT}:localhost:${OPENEDAI_PORT} \
+    -R ${TUNNEL_REMOTE_PORT_RPC}:localhost:${OPENEDAI_PORT:-5001} \
     -i "/app/${TUNNEL_KEY_NAME}" \
-    ${TUNNEL_SERVER}
+    ${TUNNEL_SERVER_RPC}
   RET_SSH=$?
 
   if [ ${RET_SSH} -ne 0 ]; then
-    echo Cannot establish the channel to ${TUNNEL_SERVER}
+    echo Cannot establish the channel to ${TUNNEL_SERVER_RPC}
     tail -f /dev/null
   fi
 
-  echo Established the channel to ${TUNNEL_SERVER}
+  echo Established the channel to ${TUNNEL_SERVER_RPC}
+fi
+
+if [[ "${TUNNEL_REMOTE_PORT_STREAM}" != "" ]]; then
+  echo Connecting to ${TUNNEL_SERVER_STREAM}...
+
+  ssh -f -N -T \
+    -o StrictHostKeyChecking=no \
+    -R ${TUNNEL_REMOTE_PORT_STREAM}:localhost:${CONTAINER_API_STREAM_PORT:-5005} \
+    -i "/app/${TUNNEL_KEY_NAME}" \
+    ${TUNNEL_SERVER_STREAM}
+  RET_SSH=$?
+
+  if [ ${RET_SSH} -ne 0 ]; then
+    echo Cannot establish the channel to ${TUNNEL_SERVER_STREAM}
+    tail -f /dev/null
+  fi
+
+  echo Established the channel to ${TUNNEL_SERVER_STREAM}
 fi
 
 if [[ "${MODEL}" == "NULL" ]]; then
